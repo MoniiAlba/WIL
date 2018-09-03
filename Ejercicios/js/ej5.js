2aa953e9fa49658bcf9c0d07e0eb221091342a7d
MoniiAlba <soeur_azmaria@hotmail.com>
2018-09-03 17:26:07 -0500

    ejercicioPiramides

---
+++ Ejercicios/js/ej5.js
@@ -0,0 +1,11 @@
+var linea = "";
+var linea2 = "";
+function agrega(){
+	document.getElementById("agregame").innerHTML += linea + 0 + "<br>";
+	linea+=0;
+}
+
+function agrega2(){
+	document.getElementById("agregame2").innerHTML += linea2 + 0 + "<br>";
+	linea2+=0;
+}
